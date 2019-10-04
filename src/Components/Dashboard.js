cce15a463295b1d69c1d38404d3785603423cbad
Nathalie ROBERT <robert.nathaliejm@gmail.com>
2019-10-04 13:03:16 +0200

    design changes

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -9,10 +9,11 @@ import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import {CTX} from './Store';
-import logo from '../img/Lycos.jpg';
+import logo from '../img/LycosLogo.jpg';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    background: 'radial-gradient(rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)',
       margin: '5%',
       marginLeft: '20%',
       marginRight: '20%',
